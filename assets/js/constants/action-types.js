cbee7c876af4ea32a717e2eae91a951edceb5ecc
Ben Howdle <benhowdle89@gmail.com>
2017-02-27 15:37:47 +0000

    client js hooked in

--- assets/js/constants/action-types.js
+++ assets/js/constants/action-types.js
@@ -3,10 +3,6 @@ export const LOGIN_FAILED = 'LOGIN_FAILED'
 export const LOGGED_IN = 'LOGGED_IN'
 export const LOGOUT = 'LOGOUT'
 
-export const SIGN_UP_INIT = 'SIGN_UP_INIT'
-export const SIGN_UP_FAILED = 'SIGN_UP_FAILED'
-export const SIGNED_UP = 'SIGNED_UP'
-
 export const SAVE_SETTINGS = 'SAVE_SETTINGS'
 export const SAVING_SETTINGS = 'SAVING_SETTINGS'
 export const SAVED_SETTINGS = 'SAVED_SETTINGS'
--- assets/js/reducers/index.js
+++ assets/js/reducers/index.js
@@ -1,17 +1,7 @@
 import authState from './auth'
-import pageState from './page'
-import themesState from './theme'
-import formsState from './form'
-import billingState from './billing'
-import domainState from './domain'
 
 const reducers = {
-    authState,
-    pageState,
-    themesState,
-    billingState,
-    domainState,
-    ...formsState
+    authState
 }
 
 export default reducers
