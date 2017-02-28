2c86e495d90e12a1a4e98e1fba01649a254f8169
Ben Howdle <benhowdle89@gmail.com>
2017-02-28 17:52:20 +0000

    homepage

--- assets/js/components/dashboard/stripe.jsx
+++ assets/js/components/dashboard/stripe.jsx
@@ -5,11 +5,6 @@ const Stripe = ({ onToken }) => {
     return (
         <div className="center">
             <p className="bold mb2">Pay $5 and activate your account for life</p>
-            <ul className="mb2 left-align max-width-1 mx-auto">
-                <li><span>{`\u2014`}</span>No hidden fees</li>
-                <li><span>{`\u2014`}</span>One-off payment</li>
-                <li><span>{`\u2014`}</span>Lists are better in 3's</li>
-            </ul>
             <StripeCheckout
                 token={onToken}
                 stripeKey={config.STRIPE_TOKEN}
--- assets/js/components/home/index.jsx
+++ assets/js/components/home/index.jsx
@@ -10,21 +10,17 @@ class Home extends React.Component {
         const loggedIn = this.props.authState.logged_in
         return (
             <div className="center">
-                <div className="sell mb2">
-                    <p className="h3 mb1">Sick of a distracting timeline?</p>
-                    <p className="h3 mb1">Tempted to delete Twitter altogether?</p>
-                    <p className="mb1"><span className="italic">Could do</span>...</p>
-                    <p className="mb1">But what if someone wants to hire you? Or see if you're available for freelance work? It would be nice to see <span className="italic">those</span> messages.</p>
-                    <p>And it would well and truly <span className="bold">suck</span> to miss those opportunities.</p>
+                <div className="mb3 center">
+                    <p className="h3">Sick of a distracting timeline?</p>
+                    <p className="h3">Tempted to delete Twitter altogether?</p>
                 </div>
 
-                <div className="blurb mb4">
-                    <p className="mb2"><span className="bold">Disconnect Today</span> sends you an email digest of your recent @mentions and DMs, on a schedule that suits you.</p>
-                    <ul className="mb2 left-align max-width-1 mx-auto">
-                        <li><span>{`\u2014`}</span>No "popular tweets", or #moments.</li>
-                        <li><span>{`\u2014`}</span>Receive your email digest every day, every 2 days, or just once a week.</li>
-                        <li><span>{`\u2014`}</span>A one-off <em className="bold">$5</em> payment activates your account for life.</li>
-                    </ul>
+                <div className="sell mb2">
+                    <p>You could, but what if someone wants to hire you? <br />What if you're available for freelance work?<br/> It would be nice to see those messages. <br/>It would well and truly suck to miss those opportunities.</p>
+                </div>
+
+                <div className="blurb mb3">
+                    <p className="mb2">Disconnect Today sends you an email digest of your recent @mentions and DMs, no popular tweets or #moments, on a schedule that suits you.</p>
                 </div>
 
                 {
@@ -35,8 +31,8 @@ class Home extends React.Component {
                 {
                     !loggedIn && (
                         <div>
-                            <p className="mb2">Sound good?</p>
-                            <a className="button" href="/api/twitter-connect">Sign in with Twitter</a>
+                            <p className="bold">A one-off $5 payment activates your account for life.</p>
+                            <p>Sound good? <a className="button" href="/api/twitter-connect">Sign in with Twitter</a></p>
                         </div>
                     )
                 }
