36b1f577a68fe2b1844e668fa43a73f509f37e69
Ben Howdle <benhowdle89@gmail.com>
2017-02-28 18:52:02 +0000

    footer

--- assets/js/components/footer.jsx
+++ assets/js/components/footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default () => {
     return (
-        <div className="py2 mt4 footer">
-            <p className="left"><a className="button" href="mailto:hello@disconnect.today">Support</a></p>
-            <p className="right">{ new Date().getFullYear() }</p>
+        <div className="py2 mt4 footer flex">
+            <p className="flex-auto"><a className="button" href="mailto:hello@disconnect.today">Support</a></p>
+            <p>{ new Date().getFullYear() }</p>
         </div>
     )
 }
