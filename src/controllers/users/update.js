7547c441e2e6a4e8fe91e4c917841bdf534593f4
Ben Howdle <benhowdle89@gmail.com>
2017-02-28 11:12:02 +0000

    validation

--- src/controllers/users/update.js
+++ src/controllers/users/update.js
@@ -5,6 +5,12 @@ import ApiError from './../../etc/error'
 
 const update = async ({ user, email, frequency }) => {
     const users = new Users()
+    if(!email) {
+        throw new ApiError(400, 'Please fill in your email address')
+    }
+    if(['day', '2_days', 'week'].indexOf(frequency) === -1) {
+        throw new ApiError(400, 'A valid frequency is required')
+    }
     const updatedUser = await users.updateSettings(user.id, email, frequency)
     return {
         user: updatedUser
