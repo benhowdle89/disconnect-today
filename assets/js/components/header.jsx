0d0bca37d529b3012af75eb67f23cb0c13133bd5
Ben Howdle <benhowdle89@gmail.com>
2017-02-28 15:59:50 +0000

    copy

--- assets/js/components/header.jsx
+++ assets/js/components/header.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router'
 
 export default () => {
     return (
-        <div>
-            <h1 className="logo center my4"><Link to="/"><span className="first">Disconnect</span> <span className="second">Today</span></Link></h1>
+        <div className="center my4">
+            <h1 className="logo"><Link to="/"><span className="first">Disconnect</span> <span className="second">Today</span></Link></h1>
+            <p style={{
+                color: 'rgba(34, 34, 34, 0.7)'
+            }}>Twitter digest emails without the distraction</p>
         </div>
     )
 }
