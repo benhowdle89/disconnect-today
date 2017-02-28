345a491d6d916cf5da7481d3a7bf11063fb7637b
Ben Howdle <benhowdle89@gmail.com>
2017-02-28 16:37:23 +0000

    favicon

--- assets/js/components/footer.jsx
+++ assets/js/components/footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default () => {
     return (
-        <div className="center py2 mt4 footer">
-            <p>Made by <a className="button" href="http://benhowdle.im">Ben Howdle</a></p>
+        <div className="py2 mt4 footer">
+            <p className="left"><a className="button" href="mailto:hello@disconnect.today">Support</a></p>
+            <p className="right">{ new Date().getFullYear() }</p>
         </div>
     )
 }
